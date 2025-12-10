6bb4cff094518f72dcb5b0fb4dd52f7d06cd09cb
T.Danev <danevtm@gmail.com>
2025-12-10 19:05:25 +0200

    Refactor ProfileCard Component Structure

--- client/src/components/profile/profile-card/ProfileCard.jsx
+++ client/src/components/profile/profile-card/ProfileCard.jsx
@@ -1,42 +1,25 @@
-import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
-import { ENDPOINT_PROFILES } from '../../../config/api.js';
-import request from '../../../utils/requester.js';
 import LogoSpinner from '../../ui/spinner/LogoSpinner.jsx';
 
-export default function ProfileCard() {
-    const { userId } = useParams();
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        request(`${ENDPOINT_PROFILES}${userId}`)
-            .then(data => setUser(data))
-            .catch(error => alert(error.message));
-    }, [userId]);
-
+export default function ProfileCard({ user }) {
     if (!user) return <LogoSpinner />;
     const { username, avatar, aboutMe } = user;
     const avatarSrc = `/images/avatars/${avatar}.svg`;
 
     return (
-        <>
-            <div className='bg-gradient-to-r from-white via-gray-300 to-gray-400 w-[380px] h-[240px] rounded-xl shadow-md border-b-8 border-black border-r-6 border-gray-900 relative p-6 z-10 pointer-events-auto'>
-                <div className='flex h-full items-center gap-6'>
-                    <div className='flex flex-col items-center -mt-4'>
-                        <img
-                            src={avatarSrc}
-                            alt={username}
-                            className='w-28 h-28 rounded-full'
-                        />
-                        <p className='font-semibold text-black mt-2'>
-                            {username}
-                        </p>
-                    </div>
-                    <p className='flex-1 text-center text-sm text-gray-900 px-2'>
-                        {aboutMe}
-                    </p>
+        <div className='bg-gradient-to-r from-white via-gray-300 to-gray-400 w-[380px] h-[240px] rounded-xl shadow-md border-b-8 border-black border-r-6 border-gray-900 relative p-6 z-10 pointer-events-auto'>
+            <div className='flex h-full items-center gap-6'>
+                <div className='flex flex-col items-center -mt-4'>
+                    <img
+                        src={avatarSrc}
+                        alt={username}
+                        className='w-28 h-28 rounded-full'
+                    />
+                    <p className='font-semibold text-black mt-2'>{username}</p>
                 </div>
+                <p className='flex-1 text-center text-sm text-gray-900 px-2'>
+                    {aboutMe}
+                </p>
             </div>
-        </>
+        </div>
     );
 }
